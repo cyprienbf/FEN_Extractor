cd5a3ddcb7d2cc37deaa23efdc938285b3b43938
cyprienbf <noreply@cyprien.ovh>
2026-01-04 12:38:49 +0100

    1.0

---
+++ userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        FEN_Extractor
+// @namespace   FEN_Extractor
+// @version     1.0
+// @description Adds a button to copy the FEN on Lichess and Chess.com
+// @author      cyprienbf
+// @match       https://lichess.org/*
+// @match       https://www.chess.com/*
+// @grant       GM_setClipboard
+// @grant       unsafeWindow
+// @run-at      document-idle
+// ==/UserScript==
